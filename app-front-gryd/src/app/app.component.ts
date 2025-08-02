ba445f81b403a220fed7a286f21dcf74e980d954
Julia Reis <1706juliareis@gmail.com>
2025-08-02 02:51:08 -0300

    estrutura da section de beneficios

--- app-front-gryd/src/app/app.component.ts
+++ app-front-gryd/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MotivosComponent } from './components/section/motivos/motivos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports:[MotivosComponent]
 })
 export class AppComponent {
   title = 'app-front-gryd';
